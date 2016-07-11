bf5617f93d2fa6def97e1a56d69975d63b2d5655
Miguel Péres <foxmpc@gmail.com>
2016-07-11 16:08:27 -0300

    Teste de dontpad.

--- js/ajudaong.js
+++ js/ajudaong.js
@@ -1,4 +1,11 @@
 function writeToDontpad() {
 	console.log("Enviando para dontpad...");
-	$.post( "http://dontpad.com/ajudaong/log.body.json", { text: "teste" } );
+	$.post( "http://dontpad.com/ajudaong/log.body.json", { text: "github" } );
+}
+
+function readFromDontpad() {
+	console.log('Lendo do dontpad...');
+	$.get( "http://dontpad.com/ajudaong/log.body.json", function( data ) {
+  		console.log( data );
+});
 }
