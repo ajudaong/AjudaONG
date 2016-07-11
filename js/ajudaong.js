824939499c081749dcfb42e481b74a3c1580de89
allmonty <allanbrados@gmail.com>
2016-07-11 20:00:51 -0300

    Retira heroku para testar

--- js/ajudaong.js
+++ js/ajudaong.js
@@ -27,14 +27,14 @@ function writeToDontpad(donationValue) {
 }
 
 function readFromDontpad(callback) {
-	$.ajaxPrefilter( function (options) {
-		  if (options.crossDomain && jQuery.support.cors) {
-		    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
-		    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-		    //options.url = "http://cors.corsproxy.io/url=" + options.url;
-		  }
-	});
-
+	// $.ajaxPrefilter( function (options) {
+	// 	  if (options.crossDomain && jQuery.support.cors) {
+	// 	    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+	// 	    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+	// 	    //options.url = "http://cors.corsproxy.io/url=" + options.url;
+	// 	  }
+	// });
+	
 	$("#temp-storage").load('http://dontpad.com/!/266af60a32285a665372e6e8670db2cb20ea1277', function() {
 		var dontpadContent = document.getElementById('temp-storage').innerHTML;
 		callback(dontpadContent);
