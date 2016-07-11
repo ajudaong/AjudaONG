31384a7089eacb4fdeafd5de2c4091acf9629f3c
Miguel Péres <foxmpc@gmail.com>
2016-07-11 16:45:36 -0300

    Testando leitura do dontpad.

--- js/ajudaong.js
+++ js/ajudaong.js
@@ -1,12 +1,18 @@
 function writeToDontpad() {
-	console.log("Enviando para dontpad...");
-	$.post( "http://dontpad.com/ajudaong/log.body.json", { text: "github" } );
+	// console.log("Enviando para dontpad...");
+	// $.post( "http://dontpad.com/ajudaong/log.body.json", { text: "github" } );
+
+	console.log('Lendo do dontpad...');
+	$.get( "http://dontpad.com/ajudaong/log.body.json", function( data ) {
+  		console.log( data );
+	});
+
 }
 
 function readFromDontpad() {
 	console.log('Lendo do dontpad...');
 	$.get( "http://dontpad.com/ajudaong/log.body.json", function( data ) {
   		console.log( data );
-});
+	});
 }
 
