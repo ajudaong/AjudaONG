8c4dd86f7f0f5d5b6458fb62f6a6ab2f933d52c4
Miguel Péres <foxmpc@gmail.com>
2016-07-11 17:31:24 -0300

    Recupera dados do dontpad

--- js/ajudaong.js
+++ js/ajudaong.js
@@ -3,14 +3,17 @@ function writeToDontpad() {
 	$.post( "http://dontpad.com/ajudaong/log.body.json", { text: "github" } );
 }
 
-function readFromDontpad() {
+function readFromDontpad(callback) {
 	$.ajaxPrefilter( function (options) {
 		  if (options.crossDomain && jQuery.support.cors) {
 		    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
 		    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
 		    //options.url = "http://cors.corsproxy.io/url=" + options.url;
 		  }
-		});
+	});
 
-		$("#loaded").load('http://dontpad.com/!/266af60a32285a665372e6e8670db2cb20ea1277');		
+	$("#temp-storage").load('http://dontpad.com/!/266af60a32285a665372e6e8670db2cb20ea1277', function() {
+		dontpadContent = document.getElementById('temp-storage').innerHTML;
+		callback();
+	});
 }
